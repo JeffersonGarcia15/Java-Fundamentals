4f9204f94a8069cb0b42e05c6d4283ad11ce0bb2
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2023-08-11 18:54:50 -0500

    QuotaCalculator

--- src/main/java/decision_structure/QuotaCalculator.java
+++ src/main/java/decision_structure/QuotaCalculator.java
@@ -1,24 +1,23 @@
 package decision_structure;
+
 import java.util.Scanner;
 
-public class SalaryCalculator {
+public class QuotaCalculator {
     public static void main(String[] args) {
         // What we know
-        double salary = 1000;
-        double bonus = 250;
         int quota = 10;
 
         System.out.println("How many sales did the employee make this week?");
         Scanner scanner = new Scanner(System.in);
         int sales = scanner.nextInt();
 
-        if (sales > quota) {
-            salary += bonus;
+        if (sales >= quota) {
+            System.out.println("Congrats! You've met your quota.");
+        } else {
+            int salesShort = quota - sales;
+            System.out.println("You did not make your quota. You were " + salesShort + " sales short.");
         }
 
         scanner.close();
-
-        System.out.println("The employee's pay is $" + salary);
-
     }
 }
