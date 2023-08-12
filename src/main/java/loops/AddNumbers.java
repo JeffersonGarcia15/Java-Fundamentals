74a0049c0dbe937d74820dc304c8ba4b1a03f8ef
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2023-08-12 13:54:47 -0500

    AddNumber

---
+++ src/main/java/loops/AddNumbers.java
@@ -0,0 +1,22 @@
+package loops;
+
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean again;
+
+        do {
+            System.out.println("Enter the first number");
+            double num1 = scanner.nextDouble();
+            System.out.println("Enter the second number");
+            double num2 = scanner.nextDouble();
+
+            System.out.println("The sum is: " + (num1 + num2));
+
+            System.out.println("Would you like to play again? false for no and true for yes");
+            again = scanner.nextBoolean();
+        } while (again);
+    }
+}
