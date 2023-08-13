86b463959ccad657feefdc456baa00f3cf45d5de
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2023-08-13 14:08:41 -0500

    addSpaces

--- src/main/java/strings/TextProcessor.java
+++ src/main/java/strings/TextProcessor.java
@@ -5,7 +5,11 @@ public class TextProcessor {
 
     public static void main(String[] args) {
         //countWords("I love Java");
-        reverseString("stop"); // "pots"
+        //reverseString("stop"); // "pots"
+        addSpaces("HeyWorld!It'sMeJefferson");
+        addSpaces("");
+        addSpaces("helloEveryoneAndBye");
+
     }
 
     private static void countWords(String text) {
@@ -26,6 +30,38 @@ public class TextProcessor {
         }
 
     }
+//    private static void addSpaces(String text) {
+//        if (text.isEmpty()) {
+//            System.out.println("Sorry, your string is empty!");
+//            return;
+//        }
+//        StringBuilder str = new StringBuilder();
+//        // Check if the first word is upper case and if it is .append it to str.
+//        str.append(text.charAt(0)); // "H"
+//
+//        for (int i = 1; i < text.length(); i++) {
+//            if (Character.isUpperCase(text.charAt(i))) {
+//                str.append(" ");
+//                str.append(text.charAt(i)); // "Hey W
+//            } else {
+//                str.append(text.charAt(i)); // "Hey | "Hey World!
+//            }
+//        }
+//        System.out.print(str);
+//    }
+
+    private static void addSpaces(String text) {
+        var modifiedText = new StringBuilder(text);
+        for (int i = 0; i < text.length(); i++) {
+            if (i != 0 && Character.isUpperCase(modifiedText.charAt(i))) {
+                modifiedText.insert(i, " "); // World!It'sMeJefferson is shifted the first time we enter here by one unit to make space for the " "
+                i++; // We want to do this because where we had an upper case we just inserted a white space.
+                // So we increment i so that we go to where we would've gone if we didn't insert that white space there.
+            }
+        }
+        System.out.println(modifiedText);
+    }
+
 
 
 }
