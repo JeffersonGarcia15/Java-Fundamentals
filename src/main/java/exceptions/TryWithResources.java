353e3ac49557fe37f191ec655e4211a764654478
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2023-08-16 09:48:41 -0500

    More on try/catch

--- src/main/java/exceptions/TryWithResources.java
+++ src/main/java/exceptions/TryWithResources.java
@@ -2,16 +2,22 @@ package exceptions;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TryWithResources {
 
     public static void main(String[] args) {
-        File file = new File("files/numbers.txt");
-        try (Scanner fileReader  = new Scanner(file)) { // only sub-classes of the autocloseable class can be here
+        File inputFile = new File("files/numbers.txt");
+        File outputFile = new File("files/output.txt");
+        try (
+                Scanner fileReader  = new Scanner(inputFile);
+                PrintWriter fileWriter = new PrintWriter(outputFile); // These are closed in REVERSE order
+        ) { // only subclasses of the autocloseable class can be here
             while (fileReader.hasNext()) {
-                System.out.println("The number is: " + fileReader.nextDouble());
+//                System.out.println("The number is: " + fileReader.nextDouble());
+                fileWriter.println(fileReader.nextDouble());
             }
         } catch (FileNotFoundException | InputMismatchException error) {
             System.out.println(error.getMessage());
